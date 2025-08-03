031737866028d2631b239631635e4bc0df81665d
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-02 20:56:38 -0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ async function main() {
   console.log("Deploying with:", deployer.address);
 
   const PorkElon = await hre.ethers.getContractFactory("PorkElon");
-  const contract = await PorkElon.deploy("0xYourMarketingWalletHere");
+  const contract = await PorkElon.deploy("0xcf3a14574983317e2339e8b28e9e5caf1e854ba4");
 
   await contract.deployed();
   console.log("PorkElon deployed to:", contract.address);
