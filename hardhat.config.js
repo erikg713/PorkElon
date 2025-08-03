6fd20dbece87fe128d3acc3388f5b8f0e9799219
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-02 21:12:24 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,16 @@
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    goerli: {
+      url: process.env.RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
