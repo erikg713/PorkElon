ba6db5816e98036957f558e96f5088b312debaf0
Dev713 <53387865+erikg713@users.noreply.github.com>
2025-08-06 18:34:54 -0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,11 +6,11 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     goerli: {
-      url: process.env.RPC_URL,
+      url: process.env.GOERLI_RPC_URL,
       accounts: [process.env.PRIVATE_KEY],
-    }
+    },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
